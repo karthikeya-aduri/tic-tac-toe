3f4b1c8435cd13c5d4496f7ccc4763e7571768f2
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-09-01 18:48:15 +0530

    Added binarySearch function for obtaining indices Renamed game() to runGame() Inputs from the bot and user are taken through console

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-function game() {
-
+function binarySearch(arr, target) {
+    let start = 0, end = arr.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (arr[mid] === target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    return undefined;
 }
 
-game();
+function runGame() {
+    const user = (prompt('Enter your choice (X or O)') === 'X') ? 1 : 2;
+    const comp = (user === 1) ? 2 : 1;
+    let boardSize = 9;
+    let board = new Array(boardSize).fill(0);
+    let remaining = Array.from({ length: boardSize }, (_, index) => index);
+    let userTurn = (user === 1) ? true : false;
+    while (boardSize !== 0) {
+        if (userTurn === true) {
+            let userChoice = Number(prompt('Enter the index to place your input (0 - 8) :'));
+            if (remaining.includes(userChoice) === false) {
+                console.log('Invalid index given');
+                continue;
+            }
+            board[userChoice] = user;
+            let temp = binarySearch(remaining, userChoice);
+            remaining.splice(temp, 1);
+            boardSize--;
+            userTurn = !userTurn;
+        }
+        else {
+            let compChoice = Math.floor(Math.random() * remaining.length);
+            board[remaining[compChoice]] = comp;
+            remaining.splice(compChoice, 1);
+            boardSize--;
+            userTurn = !userTurn;
+        }
+    }
+}
+
+runGame();
